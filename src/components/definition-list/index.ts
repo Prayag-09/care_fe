b29733035c84b568af76e58b6b9ba304120bd280
yash-learner <yash.topaz@gmail.com>
2025-05-26 18:09:42 +0530

    Add extracted FIlterTab
    -  So that can it can re-used similar to FilterSelect
    - Todo: Later move FilterSelect & FIlterTab to a common UI folder from definition list

---
+++ src/components/definition-list/index.ts
@@ -0,0 +1,5 @@
+export { EmptyState } from "./EmptyState";
+export { FilterSelect } from "./FilterSelect";
+export { FilterTabs } from "./FilterTabs";
+
+// Todo: Mobe FIlterSelect and FilterTabs to coomon UI components
