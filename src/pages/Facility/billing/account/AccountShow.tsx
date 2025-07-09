c88e99fac3de93bda1b2e9a0fbe4a0127c1bda73
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2025-07-09 18:35:34 +0530

    Update AccountShow component to improve active tab styling
    - Adjusted Tailwind CSS classes for active tab states to ensure consistent background styling across tabs.
    - Removed unnecessary background color changes for active states to enhance visual clarity.

--- src/pages/Facility/billing/account/AccountShow.tsx
+++ src/pages/Facility/billing/account/AccountShow.tsx
@@ -455,19 +455,19 @@ export function AccountShow({
           <TabsList className="border-b border-gray-300 w-full flex justify-start gap-0 rounded-none bg-transparent p-0">
             <TabsTrigger
               value="invoices"
-              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-gray-100 data-[state=active]:font-semibold text-gray-600"
+              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-transparent data-[state=active]:font-semibold text-gray-600"
             >
               {t("invoices")}
             </TabsTrigger>
             <TabsTrigger
               value="charge_items"
-              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-gray-100 data-[state=active]:font-semibold text-gray-600"
+              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-transparent data-[state=active]:font-semibold text-gray-600"
             >
               {t("charge_items")}
             </TabsTrigger>
             <TabsTrigger
               value="payments"
-              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-gray-100 text-gray-600"
+              className="border-b-2 px-6 py-2 text-sm font-medium data-[state=active]:border-b-primary-700 data-[state=active]:text-primary-800 rounded-none bg-transparent data-[state=active]:shadow-none data-[state=active]:bg-transparent text-gray-600"
             >
               {t("payments")}
             </TabsTrigger>
