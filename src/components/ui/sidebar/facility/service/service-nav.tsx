f4bfa85516e72f3ce6f927323fe0ce158e27d12b
Jacob Jeevan <mail@jacobjeevan.me>
2025-08-27 18:03:42 +0530

    add service layout and switcher

--- src/components/ui/sidebar/facility/service/service-nav.tsx
+++ src/components/ui/sidebar/facility/service/service-nav.tsx
@@ -5,22 +5,20 @@ import CareIcon from "@/CAREUI/icons/CareIcon";
 import { NavMain } from "@/components/ui/sidebar/nav-main";
 
 import useCurrentService from "@/pages/Facility/services/utils/useCurrentService";
-import useCurrentFacility from "@/pages/Facility/utils/useCurrentFacility";
 
 export function ServiceNav() {
   const { t } = useTranslation();
 
-  const { facility } = useCurrentFacility();
-  const { service } = useCurrentService();
+  const { service, facilityId } = useCurrentService();
 
-  const baseUrl = `/facility/${facility?.id}/services/${service?.id}`;
+  const baseUrl = `/facility/${facilityId}/services/${service?.id}`;
 
   return (
     <NavMain
       links={[
         {
           name: t("locations"),
-          url: `${baseUrl}/`,
+          url: `${baseUrl}/locations`,
           icon: <CareIcon icon="l-map-pin" />,
         },
         {
